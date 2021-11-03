12b7362b5ab25d3e8411290513811bc144b4e37a
stsaha <91664332+stsaha@users.noreply.github.com>
2021-11-03 00:35:18 -0700

    "update"

---
+++ lab4/js/lab.js
@@ -0,0 +1,22 @@
+// Author: Sohinee Saha
+// Username: stsaha
+// Description. This is a career-finding program.
+// Year: 2021
+
+// PSEUDOCODE
+// 1. List out possible careers incorporated in the program.
+//    - Careers = teacher, lawyer, doctor, engineer, cook, artist, musician, scientist, entrepreneur, activist
+// 2. Create an array with variables for each career.
+//    - Integer Variables = teacher, lawyer, doctor, engineer, cook, artist, musician, scientist, entrepreneur, activist
+// 3. Ask the user several questions to understand their interests.
+//    - Prepare list of 10-15 questions.
+// 4. Depending on input, increment career-related variables in the array.
+//    - If answer  shows no releation to career type, do not increment.
+//    - If answer shows slight relation to career type, increment by +1.
+//    - If answer shows strong relation to career type, increment by +2.
+// 5. Variable with greatest value in the array at the end is the chosen career.
+//    - Declare max variable and initalize to the value of the first element in array.
+//    - Use a for loop to go through entire array.
+//    - If value of current element is greater than current max, replace max with value of current element.
+// 6. Deliver results to the user.
+//    - Use a print statement.
