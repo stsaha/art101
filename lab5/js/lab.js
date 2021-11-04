a4b178f40165aed105f37cd2f2a88d91ea1146a5
stsaha <91664332+stsaha@users.noreply.github.com>
2021-11-03 20:51:05 -0700

    "pushing lab 5"

---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+/*
+Author: Sohinee Saha
+Email: stsaha@ucsc.edu
+Assignment: Art 101, Lab 5
+Created: October 2021
+*/
+
+//Define and initialize variables
+var make = "Toyota";
+var model = "Highlander";
+var color = "Bluestone Metallic";
+var year = 2004;
+
+//Define and calculate variable for car's age
+var age = 2021 - year;
+
+//Creating a boolean variable
+var ownIt = true;
+
+//Deliver output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Own It: " + ownIt);
