5e18b7048985055a380c68461ff522e876648b22
stsaha <91664332+stsaha@users.noreply.github.com>
2021-11-14 19:09:58 -0800

    pushing to server

---
+++ lab7/js/lab.js
@@ -0,0 +1,31 @@
+/*
+Author: Sohinee Saha
+Email: stsaha@ucsc.edu
+Assignment: Art 101, Lab 7
+Created: October 2021
+*/
+
+
+function sortUserName()
+{
+  //take string name as user input
+  var userName = window.prompt("Hello! What is your name?");
+  userName = userName.toLowerCase();
+  console.log ("userName =", userName);
+
+  //split string by characters and into array
+  var nameArray = userName.split('');
+  console.log ("name array =", nameArray);
+
+  //sort characters in array in alphabetical order
+  var sortArray = nameArray.sort();
+  console.log("sorted array =", sortArray);
+
+  //join characters in array
+  var joinArray = sortArray.join('');
+  console.log("final array =", joinArray);
+
+  return joinArray;
+}
+
+document.writeln("This would be your name if all the letters were in alphabetical order: ", sortUserName(), "</br>");
